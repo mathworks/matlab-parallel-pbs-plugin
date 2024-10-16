7b4250b60e178ca075fe035759fcb1db11d4ee2d
James Cave <jcave@mathworks.com>
2024-10-16 11:23:42 +0100

    v2.2.1

--- independentJobWrapper.sh
+++ independentJobWrapper.sh
@@ -5,7 +5,7 @@
 # PARALLEL_SERVER_MATLAB_EXE  - the MATLAB executable to use
 # PARALLEL_SERVER_MATLAB_ARGS - the MATLAB args to use
 
-# Copyright 2010-2022 The MathWorks, Inc.
+# Copyright 2010-2024 The MathWorks, Inc.
 
 # If PARALLEL_SERVER_ environment variables are not set, assign any
 # available values with form MDCE_ for backwards compatibility
@@ -14,6 +14,11 @@ PARALLEL_SERVER_MATLAB_ARGS=${PARALLEL_SERVER_MATLAB_ARGS:="${MDCE_MATLAB_ARGS}"
 
 # Echo the node that the scheduler has allocated to this job:
 echo "The scheduler has allocated the following node to this job: `hostname`"
+
+# PBS will set TMPDIR to a folder it will create under the /var/tmp folder,
+# but on slow filesystems we might try to use the folder before we see it's
+# been created. Set TMPDIR back to /tmp here to avoid this.
+export TMPDIR=/tmp
 
 if [ ! -z "${PBS_ARRAY_INDEX}" ] ; then
     # Use job arrays
